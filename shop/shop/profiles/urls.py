4327a21ec8264c8d6002ceb6ee61939255f560f0
nikolaynikolov971 <nikolov.nikolay971@gmail.com>
2021-09-12 10:58:58 +0300

    update all_profiles url

--- shop/shop/profiles/urls.py
+++ shop/shop/profiles/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from shop.profiles.views import home_page, my_profile, profile_details, show_all_profiles
+from shop.profiles.views import home_page, my_profile, profile_details, ShowAllProfiles
 
 urlpatterns = [
     path('', home_page, name='landing_page'),
     path('my_profile/', my_profile, name='my_profile'),
     path('profile_details', profile_details, name='profile_details'),
-    path('all_profiles/', show_all_profiles, name='all_profiles')
+    path('all_profiles/', ShowAllProfiles.as_view(), name='all_profiles')
 
 
 ]
