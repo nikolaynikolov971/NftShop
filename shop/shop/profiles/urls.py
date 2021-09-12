07400fe304a290b4b82f8c8007199d8c9c953e31
nikolaynikolov971 <nikolov.nikolay971@gmail.com>
2021-09-12 10:30:30 +0300

    add all profiles url

--- shop/shop/profiles/urls.py
+++ shop/shop/profiles/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from shop.profiles.views import home_page, my_profile, profile_details
+from shop.profiles.views import home_page, my_profile, profile_details, show_all_profiles
 
 urlpatterns = [
     path('', home_page, name='landing_page'),
     path('my_profile/', my_profile, name='my_profile'),
     path('profile_details', profile_details, name='profile_details'),
+    path('all_profiles/', show_all_profiles, name='all_profiles')
 
 
 ]
